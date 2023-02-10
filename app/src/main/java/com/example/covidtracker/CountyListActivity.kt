ee7cb71fc32f49aadbdf74febc8c3d80d2af6ad8
Ryan <ryanestanislao@gmail.com>
2023-02-10 09:32:30 -0800

    2/10/23 part 2

--- app/src/main/java/com/example/covidtracker/CountyListActivity.kt
+++ app/src/main/java/com/example/covidtracker/CountyListActivity.kt
@@ -1,20 +1,18 @@
 package com.example.covidtracker
 
-import android.net.wifi.rtt.CivicLocationKeys.STATE
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.covidtracker.databinding.ActivityCountyListBinding
-import com.example.covidtracker.CountyAdapter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class CountyListActivity : AppCompatActivity() {
     companion object{
@@ -63,10 +61,11 @@ class CountyListActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection
         return when (item.itemId) {
+
             R.id.menu_Transmission -> {
-                adapter.dataSet = adapter.dataSet.sortedByDescending {
+                adapter.dataSet = adapter.dataSet.sortedWith(compareByDescending<CountyData> {
                     it.cdcTransmissionLevel
-                }
+                }.thenBy { it.county })
                 adapter.notifyDataSetChanged()
                 true
             }
@@ -75,6 +74,17 @@ class CountyListActivity : AppCompatActivity() {
                     it.county
                 }
                 adapter.notifyDataSetChanged()
+                true
+            }
+            R.id.menu_info -> {
+                AlertDialog.Builder(this)
+                    .setTitle("CovidTracker")
+                    .setMessage("Red: High Transmission\nOrange:Substantial Transmission\nYellow: " +
+                            "Moderate Transmission\nBlue: Low Transmission\n\nThe number represents " +
+                            "the weekly case count per 100k people in the county.")
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
